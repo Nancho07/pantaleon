3e49a1ffd792a113ff81cb880332e4525df8d9a7
Nancho07 <hernan@imixdigital.com>
2017-07-22 14:43:06 -0600

    Add files via upload

--- www/js/app.js
+++ www/js/app.js
@@ -2246,11 +2246,17 @@ function onAppReady(){
             $("#nota_unapprove_"+value.id_nota+" span").empty();
             $("#nota_approve_"+value.id_nota+" span").append(value.positivos);
             $("#nota_unapprove_"+value.id_nota+" span").append(value.negativos);
-        }else{
+        }else if value.portada==="SI"{
             $("#approve_"+value.id_nota+" span").empty();
             $("#unapprove_"+value.id_nota+" span").empty();
             $("#approve_"+value.id_nota+" span").append(value.positivos);
             $("#unapprove_"+value.id_nota+" span").append(value.negativos);
+        }else if value.portada==="NINGUNO"{
+            alertas.contenido='Ya votaste en esta nota';
+            alertas.btnConfirma="No";
+            alertas.funcionConfirma="";
+            alertas.funcionCancela="";
+            alerta(alertas);
         }
     });
     /*************  chat   **************************/
